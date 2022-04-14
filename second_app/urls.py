3f496e0f35996e62657062cb752ffb9afcba5277
Andrey Batishev <andrey.batishev13@mail.ru>
2022-04-14 20:20:10 +0500

    second app urls added

---
+++ second_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from second_app import views
+urlpatterns = [
+
+    path('page1/', views.index,name="Page1"),
+]
--- second_project/urls.py
+++ second_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from second_app import views
+from django.conf.urls import include
+
 urlpatterns = [
-    path('second/',views.index,name="Index"),
+    path('second/', include('second_app.urls')),
     path('admin/', admin.site.urls),
 ]
